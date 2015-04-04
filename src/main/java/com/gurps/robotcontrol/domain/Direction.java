16e3c94b549ea64b8bf1bd91b3a5a07dd11b157d
Gurpiar Bassi <gurpiarbassi@Gurpiars-MacBook-Pro.local>
2015-04-04 22:25:21 +0100

    added behaviour to the enum

--- src/main/java/com/gurps/robotcontrol/domain/Direction.java
+++ src/main/java/com/gurps/robotcontrol/domain/Direction.java
@@ -2,5 +2,56 @@ package com.gurps.robotcontrol.domain;
 
 public enum Direction {
 
-	N, E, S, W;
+	N {
+		@Override
+		public Direction right() {
+			return E;
+		}
+
+		@Override
+		public Direction left() {
+			return W;
+		}
+	},
+
+	E {
+		@Override
+		public Direction right() {
+			return S;
+		}
+
+		@Override
+		public Direction left() {
+			return N;
+		}
+
+	},
+
+	S {
+		@Override
+		public Direction right() {
+			return W;
+		}
+
+		@Override
+		public Direction left() {
+			return E;
+		}
+	},
+
+	W {
+		@Override
+		public Direction right() {
+			return N;
+		}
+
+		@Override
+		public Direction left() {
+			return S;
+		}
+	};
+
+	public abstract Direction right();
+
+	public abstract Direction left();
 }
