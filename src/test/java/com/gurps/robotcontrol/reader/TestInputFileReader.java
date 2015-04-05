1b2c6eac64102aa2e407c133732e777e8cddf42e
Gurpiar Bassi <gurpiarbassi@Gurpiars-MacBook-Pro.local>
2015-04-05 01:30:10 +0100

    tests

---
+++ src/test/java/com/gurps/robotcontrol/reader/TestInputFileReader.java
@@ -0,0 +1,14 @@
+package com.gurps.robotcontrol.reader;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestInputFileReader {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/gurps/robotcontrol/validation/TestRobotInputValidator.java
@@ -0,0 +1,14 @@
+package com.gurps.robotcontrol.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRobotInputValidator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
